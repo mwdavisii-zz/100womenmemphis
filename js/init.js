ae6fe8a3a63cfafd484adea7a8c75d09c71f0ac6
Mike Davis <mwdavisii@gmail.com>
2017-10-16 13:42:06 -0500

    almost done

--- js/init.js
+++ js/init.js
@@ -3,6 +3,6 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-
+    $(".dropdown-button").dropdown();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
